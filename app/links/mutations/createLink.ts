7f1070c8fb162d779ae66432d1bfa4bc26aab2cf
Nikita Voloboev <nikita.voloboev@gmail.com>
2021-07-11 03:41:05 +0200

    .

--- app/links/mutations/createLink.ts
+++ app/links/mutations/createLink.ts
@@ -4,6 +4,7 @@ import { z } from "zod"
 
 const CreateLink = z.object({
   title: z.string(),
+  url: z.string(),
 })
 
 export default resolver.pipe(resolver.zod(CreateLink), resolver.authorize(), async (input) => {
--- app/pages/links/new.tsx
+++ app/pages/links/new.tsx
@@ -2,10 +2,14 @@ import { Link, useRouter, useMutation, BlitzPage, Routes } from "blitz"
 import Layout from "app/core/layouts/Layout"
 import createLink from "app/links/mutations/createLink"
 import { LinkForm, FORM_ERROR } from "app/links/components/LinkForm"
+import { useCurrentUser } from "app/core/hooks/useCurrentUser"
 
 const NewLinkPage: BlitzPage = () => {
   const router = useRouter()
   const [createLinkMutation] = useMutation(createLink)
+
+  // const user = useCurrentUser()
+  // console.log(user)
 
   return (
     <div className="m-auto w-1/2">
